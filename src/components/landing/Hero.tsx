f99f5f8acedaf1fad77fe982d4aa5f1ac9f35559
Rahul9214 <147570570+Rahul9214@users.noreply.github.com>
2026-02-02 00:17:27 +0530

    fix : hero section logo has been aligned from bottom

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -148,7 +148,7 @@ const Hero = () => {
                 alt="VoxaDent AI"
                 width={900}
                 height={900}
-                className="w-full h-auto"
+                className="w-full h-auto -mt-18"
               />
             </div>
           </div>
