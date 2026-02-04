4079252be018d2536238770401845e7f531506cc
Rahul9214 <147570570+Rahul9214@users.noreply.github.com>
2026-02-04 23:16:51 +0530

    fix: layout page description has been added.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "VOXADENT - AI DENTAL ASSISTANT",
   description:
-    "VoxaDent is a smart AI voice platform for Dental Clinics to manage appointments, patients, and subscriptions seamlessly.",
+    "VoxaDent is a Smart AI Voice Platform for Dental Clinics to manage appointments, patients, and subscriptions seamlessly.",
 };
 
 export default function RootLayout({
